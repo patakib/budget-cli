aa5c5484ed64c53063761eed3db32e7a7a6eb614
Balint Pataki <balint.pataki.automation@gmail.com>
2024-05-30 18:35:13 +0200

    add new record with details

--- cmd/commands.go
+++ cmd/commands.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"log"
 	"os"
+	"time"
 
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/spf13/cobra"
@@ -19,6 +20,11 @@ type BasicConfig struct {
 	Income            int32      `yaml:"income"`
 	CategoriesPlanned []Category `yaml:"categories-planned"`
 }
+
+var InsertDate string
+var InsertCategory string
+var InsertAmount int32
+var InsertComment string
 
 func createDatabase(config BasicConfig) {
 	os.Remove("./budget.db")
@@ -74,4 +80,35 @@ var CreateCmd = &cobra.Command{
 		}
 		createDatabase(config)
 	},
+}
+
+var AddCmd = &cobra.Command{
+	Use:   "add",
+	Short: "Add new expense",
+	Long: `
+		Add new expense with the following details: date, category, amount, comment.
+		Example usage: 
+			budget add --date 2024-06-05 --category car --amount 10000 --comment fuel
+			or with short flags and using today by default:
+			budget add -c car -a 10000 -m fuel
+	`,
+
+	Run: func(cmd *cobra.Command, args []string) {
+		db, err := sql.Open("sqlite3", "./budget.db")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer db.Close()
+
+		stmt, _ := db.Prepare("insert into expenses(date, category, amount, comment) values(?, ?, ?, ?)")
+		stmt.Exec(InsertDate, InsertCategory, InsertAmount, InsertComment)
+	},
+}
+
+func init() {
+	AddCmd.Flags().StringVarP(&InsertDate, "date", "d", time.Now().Format("2006-01-02"), "Date of actual transaction in YYYY-MM-DD format")
+	// TODO: check if valid
+	AddCmd.Flags().StringVarP(&InsertCategory, "category", "c", "", "Category of transaction")
+	AddCmd.Flags().Int32VarP(&InsertAmount, "amount", "a", 0, "Transaction amount")
+	AddCmd.Flags().StringVarP(&InsertComment, "comment", "m", "", "Additional information")
 }
